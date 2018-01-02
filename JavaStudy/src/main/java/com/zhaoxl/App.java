2e998bdc89d0af6fee1e7b42d0eec56b573bf293
Evelyn <2910864240@qq.com>
2018-01-02 11:01:44 +0800

    create project

---
+++ JavaStudy/src/main/java/com/zhaoxl/App.java
@@ -0,0 +1,13 @@
+package com.zhaoxl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
